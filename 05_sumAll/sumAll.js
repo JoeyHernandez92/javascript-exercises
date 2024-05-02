46e093c222e65b32b7c9af0c554d9a7b291c56f9
JoeyHernandez92 <77796254+JoeyHernandez92@users.noreply.github.com>
2024-05-01 23:27:47 -0500

    sumAll 4 tests running

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
 const sumAll = function(num1, num2) {
     let total = 0;
+    if(num1 < 0){
+        return 'ERROR';
+    }
+    if(num1 > num2){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
     for(let i = num1; i <= num2; i++){
         total += i;
     }
