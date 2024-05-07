8537710badce2e917f8f7550bcdc1f7c151ef89d
JoeyHernandez92 <77796254+JoeyHernandez92@users.noreply.github.com>
2024-05-07 16:23:12 -0500

    tempConverision.js Done!!!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) * 5/9;
+  Math.round(temp).toFixed(1);
+  return Number(temp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * 9/5 + 32);
+  Math.round(temp).toFixed(1);
+  return Number(temp.toFixed(1));
 };
 
 // Do not edit below this line
